664021edfb5585f84aa5d294888a43cfd2799c15
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-10-03 19:21:43 +0200

    fix: Added the description for PyPi

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*-
+from pathlib import Path
+
 from setuptools import find_packages
 from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="codeboxcli",
     version="{{VERSION_PLACEHOLDER}}",
+    description='CLI for Saving and Sharing Code Snippets',
+    author='Marc Orfila Carreras',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         "sqlalchemy",
