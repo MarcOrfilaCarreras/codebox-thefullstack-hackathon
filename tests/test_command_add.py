324217c19de6e2f923c8ac3d75d30d2f44df64ea
Marc Orfila Carreras <marcorfila2003@gmail.com>
2023-10-08 13:33:59 +0200

    tests: Added new 'add' and 'delete' tests

--- tests/test_command_add.py
+++ tests/test_command_add.py
@@ -22,3 +22,42 @@ def test_add_help():
     expected_output = "Usage: codebox add [ARGS]"
 
     assert expected_output in result.stdout
+
+
+def test_add_empty_name():
+    command = ["python3", "codeboxcli/__main__.py", "add", "--name"]
+    result = subprocess.run(
+        command, capture_output=True, text=True, check=True)
+
+    expected_output = "Error: Missing value after --name."
+
+    assert expected_output in result.stdout
+
+
+def test_add_empty_description():
+    command = ["python3", "codeboxcli/__main__.py", "add", "--description"]
+    result = subprocess.run(
+        command, capture_output=True, text=True, check=True)
+
+    expected_output = "Error: Missing value after --description."
+
+    assert expected_output in result.stdout
+
+
+def test_add_empty_tags():
+    command = ["python3", "codeboxcli/__main__.py", "add", "--tags"]
+    result = subprocess.run(
+        command, capture_output=True, text=True, check=True)
+
+    expected_output = "Error: Missing value after --tags."
+
+    assert expected_output in result.stdout
+
+
+def test_add_successful_addition():
+    command = ["python3", "codeboxcli/__main__.py", "add", "--name", "pytest",
+               "--description", "This is an automatic test", "--tags", "Python3", "Cli"]
+    result = subprocess.run(
+        command, capture_output=True, text=True, check=True)
+
+    assert result.returncode == 0
--- tests/test_command_delete.py
+++ tests/test_command_delete.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import re
 import subprocess
 
 import pytest
@@ -22,3 +23,35 @@ def test_delete_help():
     expected_output = "Usage: codebox delete [ARGS]"
 
     assert expected_output in result.stdout
+
+
+def test_delete_wrong_id():
+    command = ["python3", "codeboxcli/__main__.py", "delete", "0"]
+    result = subprocess.run(
+        command, capture_output=True, text=True, check=True)
+
+    expected_output = "Error: Snippet with ID 0 not found."
+
+    assert expected_output in result.stdout
+
+
+def test_delete_successful_deletion():
+    command = ["python3", "codeboxcli/__main__.py", "list"]
+    result = subprocess.run(
+        command, capture_output=True, text=True, check=True)
+
+    # Define a regular expression pattern to match the ID column
+    id_pattern = r"\|\s+(\d+)\s+\|"
+
+    # Find all matches of the ID pattern in the output
+    id_matches = re.findall(id_pattern, result.stdout)
+
+    if len(id_matches) >= 1:
+        command = ["python3", "codeboxcli/__main__.py", "delete",
+                   id_matches[0].replace("|", "").replace(" ", "")]
+        result = subprocess.run(
+            command, capture_output=True, text=True, check=True)
+
+        assert result.returncode == 0
+    else:
+        assert 1 == 2
